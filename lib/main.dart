2ebe46b1d093b2a4f47bfdb8f7bd64241b778bc7
Mustafa Ali Dikçinar <77580742+mdikcinar@users.noreply.github.com>
2022-06-23 18:52:29 +0300

    under construction

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -14,17 +15,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Uri _url = Uri.parse('mailto:mustafaalidikcinar@hotmail.com');
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My web app test'),
-        ),
-        body: Column(
-          children: [
-            Text('My web app test'),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Builder(builder: (context) {
+        return Scaffold(
+          body: Stack(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/png/mad-designer.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.1),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Web site is under construction', style: Theme.of(context).textTheme.headline4),
+                      InkWell(
+                          onTap: () {
+                            try {
+                              launchUrl(_url);
+                            } catch (e) {
+                              throw Exception(e);
+                            }
+                          },
+                          child:
+                              Text('Contact me: mustafaalidikcinar@hotmail.com', style: Theme.of(context).textTheme.headline6)),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }),
     );
   }
 }
