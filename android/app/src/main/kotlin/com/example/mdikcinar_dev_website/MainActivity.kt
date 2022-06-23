78b017f75159dc1bc7dfd043843b79dc87384d3c
Mustafa Ali Dikçinar <77580742+mdikcinar@users.noreply.github.com>
2022-06-23 17:24:24 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mdikcinar_dev_website/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mdikcinar_dev_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My web app test'),
+        ),
+        body: Column(
+          children: [
+            Text('My web app test'),
+          ],
+        ),
+      ),
+    );
+  }
+}
